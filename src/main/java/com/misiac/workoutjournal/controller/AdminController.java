717a2cb08cfe4fc519662baed01baeb8c7099ce2
Misiac <Misiac2000@gmail.com>
2023-12-11 22:54:01 +0100

    add muscle group category post method

--- src/main/java/com/misiac/workoutjournal/controller/AdminController.java
+++ src/main/java/com/misiac/workoutjournal/controller/AdminController.java
@@ -2,6 +2,9 @@ package com.misiac.workoutjournal.controller;
 
 import com.misiac.workoutjournal.service.AdminService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,5 +19,13 @@ public class AdminController {
     @Autowired
     public AdminController(AdminService adminService) {
         this.adminService = adminService;
+    }
+
+    @PostMapping("/muscle-category")
+    public ResponseEntity<String> addMuscleGroupCategory(String newCategory) throws Exception {
+
+        adminService.addMuscleGroupCategory(newCategory);
+        return new ResponseEntity<>("Category created successfully", HttpStatus.CREATED);
+
     }
 }

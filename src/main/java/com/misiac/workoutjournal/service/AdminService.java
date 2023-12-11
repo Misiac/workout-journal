b17bbbf05977d9af116bd614b76ad93d7458ba99
Misiac <Misiac2000@gmail.com>
2023-12-11 22:55:42 +0100

    add constructor, repository fields and addMuscleGroupCategory method

--- src/main/java/com/misiac/workoutjournal/service/AdminService.java
+++ src/main/java/com/misiac/workoutjournal/service/AdminService.java
@@ -1,9 +1,33 @@
 package com.misiac.workoutjournal.service;
 
+import com.misiac.workoutjournal.entity.MuscleGroupCategory;
+import com.misiac.workoutjournal.repository.EquipmentCategoryRepository;
+import com.misiac.workoutjournal.repository.MuscleGroupCategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @Transactional
 public class AdminService {
+    private final EquipmentCategoryRepository equipmentCategoryRepository;
+    private final MuscleGroupCategoryRepository muscleGroupCategoryRepository;
+
+    @Autowired
+    public AdminService(EquipmentCategoryRepository equipmentCategoryRepository, MuscleGroupCategoryRepository muscleGroupCategoryRepository) {
+        this.equipmentCategoryRepository = equipmentCategoryRepository;
+        this.muscleGroupCategoryRepository = muscleGroupCategoryRepository;
+    }
+
+    public void addMuscleGroupCategory(String newCategoryName) throws Exception {
+
+        var muscleGroupCategory = muscleGroupCategoryRepository.findMuscleGroupCategoryByName(newCategoryName);
+
+        if (muscleGroupCategory.isPresent()) {
+            throw new Exception("Muscle Group Category already exists");
+        }
+
+        var newCategory = new MuscleGroupCategory(newCategoryName);
+        muscleGroupCategoryRepository.save(newCategory);
+    }
 }

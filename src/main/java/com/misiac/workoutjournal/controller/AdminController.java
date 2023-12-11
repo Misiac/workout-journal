3d8943d2afd4a6356d5f8bcf1c4e63e34ddc868f
Misiac <Misiac2000@gmail.com>
2023-12-11 20:02:05 +0100

    inject AdminService into controller

--- src/main/java/com/misiac/workoutjournal/controller/AdminController.java
+++ src/main/java/com/misiac/workoutjournal/controller/AdminController.java
@@ -1,5 +1,7 @@
 package com.misiac.workoutjournal.controller;
 
+import com.misiac.workoutjournal.service.AdminService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,5 +11,10 @@ import org.springframework.web.bind.annotation.RestController;
 
 public class AdminController {
 
+    private final AdminService adminService;
 
+    @Autowired
+    public AdminController(AdminService adminService) {
+        this.adminService = adminService;
+    }
 }

2e6474b7a26d544afa2ad0c421fd4437a7d1a585
Misiac <Misiac2000@gmail.com>
2023-12-11 20:00:26 +0100

    create AdminController

---
+++ src/main/java/com/misiac/workoutjournal/controller/AdminController.java
@@ -0,0 +1,13 @@
+package com.misiac.workoutjournal.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+//@CrossOrigin
+@RestController
+@RequestMapping("/api/admin")
+
+public class AdminController {
+
+
+}

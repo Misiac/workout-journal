5ec60255c9ca01df781f7c72f4db961f6a0812c6
Misiac <Misiac2000@gmail.com>
2023-12-11 14:56:06 +0100

    init

---
+++ src/test/java/com/misiac/workoutjournal/WorkoutJournalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.misiac.workoutjournal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkoutJournalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

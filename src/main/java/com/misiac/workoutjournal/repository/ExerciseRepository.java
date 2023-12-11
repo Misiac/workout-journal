7a70941c6096bf0d5e5ab89cfbef6c9cd8485091
Misiac <Misiac2000@gmail.com>
2023-12-11 19:53:50 +0100

    add exercise repository

---
+++ src/main/java/com/misiac/workoutjournal/repository/ExerciseRepository.java
@@ -0,0 +1,9 @@
+package com.misiac.workoutjournal.repository;
+
+import com.misiac.workoutjournal.entity.Exercise;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ExerciseRepository extends JpaRepository<Exercise, Integer> {
+
+    Exercise findExerciseByName(String name);
+}

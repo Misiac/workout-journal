e67151986449e39a11d65821feea23c57e8bc042
Misiac <Misiac2000@gmail.com>
2023-12-11 21:29:05 +0100

    add transactional annotation to AdminService

--- src/main/java/com/misiac/workoutjournal/service/AdminService.java
+++ src/main/java/com/misiac/workoutjournal/service/AdminService.java
@@ -1,7 +1,9 @@
 package com.misiac.workoutjournal.service;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@Transactional
 public class AdminService {
 }

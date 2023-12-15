e3006933e9fdda1f1d997e01b8340c8488cdb6b6
Misiac <Misiac2000@gmail.com>
2023-12-15 19:13:45 +0100

    disable automatic Spring data REST endpoints exposing for user and workout entities

--- src/main/java/com/misiac/workoutjournal/repository/UserRepository.java
+++ src/main/java/com/misiac/workoutjournal/repository/UserRepository.java
@@ -2,7 +2,9 @@ package com.misiac.workoutjournal.repository;
 
 import com.misiac.workoutjournal.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource(exported = false)
 public interface UserRepository extends JpaRepository<User, Integer> {
     User findUserByEmail(String email);
 }
--- src/main/java/com/misiac/workoutjournal/repository/WorkoutRepository.java
+++ src/main/java/com/misiac/workoutjournal/repository/WorkoutRepository.java
@@ -2,6 +2,8 @@ package com.misiac.workoutjournal.repository;
 
 import com.misiac.workoutjournal.entity.Workout;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource(exported = false)
 public interface WorkoutRepository extends JpaRepository<Workout, Integer> {
 }

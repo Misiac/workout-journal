f6cd6a5b846d625a928c8bc55aac7bac189155c1
Misiac <Misiac2000@gmail.com>
2023-12-15 20:18:21 +0100

    add query in ExerciseRepository for querying for specific category both native sql and JPQL versions for dev purposes

--- src/main/java/com/misiac/workoutjournal/repository/ExerciseRepository.java
+++ src/main/java/com/misiac/workoutjournal/repository/ExerciseRepository.java
@@ -2,10 +2,26 @@ package com.misiac.workoutjournal.repository;
 
 import com.misiac.workoutjournal.entity.Exercise;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface ExerciseRepository extends JpaRepository<Exercise, Integer> {
 
     Exercise findExerciseByName(String name);
 
     Exercise findExerciseById(int exerciseId);
+
+@Query(value = "SELECT e.name " +
+               "FROM exercises e " +
+               "JOIN equipment_jointable ej ON e.id = ej.exercise_id " +
+               "JOIN exercise_equipment_categories ec ON ej.category_id = ec.id " +
+               "WHERE ec.name = :name", nativeQuery = true)
+List<String> findExercisesByEquipmentCategoriesNative(String name);
+
+@Query(value = "SELECT e " +
+               "FROM Exercise e " +
+               "JOIN e.equipmentCategories ec " +
+               "WHERE ec.name = :name")
+List<Exercise> findExercisesByEquipmentCategories(String name);
 }

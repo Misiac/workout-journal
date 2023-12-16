3637a65b58c67713406b724f2615213c0b1736df
Misiac <Misiac2000@gmail.com>
2023-12-16 22:34:30 +0100

    add mapper and ExerciseRepository dependencies to AdminService with AddExercise implementation

--- src/main/java/com/misiac/workoutjournal/service/AdminService.java
+++ src/main/java/com/misiac/workoutjournal/service/AdminService.java
@@ -1,23 +1,34 @@
 package com.misiac.workoutjournal.service;
 
 import com.misiac.workoutjournal.entity.EquipmentCategory;
+import com.misiac.workoutjournal.entity.Exercise;
 import com.misiac.workoutjournal.entity.MuscleGroupCategory;
+import com.misiac.workoutjournal.mapper.ExerciseMapper;
 import com.misiac.workoutjournal.repository.EquipmentCategoryRepository;
+import com.misiac.workoutjournal.repository.ExerciseRepository;
 import com.misiac.workoutjournal.repository.MuscleGroupCategoryRepository;
+import com.misiac.workoutjournal.requestmodels.AdminCreateExerciseRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import static com.misiac.workoutjournal.requestmodels.AdminCreateExerciseRequest.*;
 
 @Service
 @Transactional
 public class AdminService {
     private final EquipmentCategoryRepository equipmentCategoryRepository;
     private final MuscleGroupCategoryRepository muscleGroupCategoryRepository;
+    private final ExerciseRepository exerciseRepository;
+    private final ExerciseMapper exerciseMapper;
 
     @Autowired
-    public AdminService(EquipmentCategoryRepository equipmentCategoryRepository, MuscleGroupCategoryRepository muscleGroupCategoryRepository) {
+    public AdminService(EquipmentCategoryRepository equipmentCategoryRepository, MuscleGroupCategoryRepository muscleGroupCategoryRepository,
+                        ExerciseRepository exerciseRepository, ExerciseMapper exerciseMapper) {
         this.equipmentCategoryRepository = equipmentCategoryRepository;
         this.muscleGroupCategoryRepository = muscleGroupCategoryRepository;
+        this.exerciseRepository = exerciseRepository;
+        this.exerciseMapper = exerciseMapper;
     }
 
     public void addMuscleGroupCategory(String newCategoryName) throws Exception {
@@ -42,5 +53,29 @@ public class AdminService {
 
         var newCategory = new EquipmentCategory(newCategoryName);
         equipmentCategoryRepository.save(newCategory);
+    }
+
+    public void addExercise(AdminCreateExerciseRequest adminExerciseRequest) throws Exception {
+        var exercise = exerciseRepository.findExerciseByName(adminExerciseRequest.getName());
+
+        //VALIDATION
+        if (exercise.isPresent()) {
+            throw new RuntimeException("Exercise already exists");
+        }
+        if (adminExerciseRequest.getName() == null) throw new Exception("Request's name is blank");
+        for (String equipmentCategory : adminExerciseRequest.equipmentCategories) {
+            if (equipmentCategoryRepository.findEquipmentCategoryByName(equipmentCategory).isEmpty()) {
+                throw new Exception("Category " + equipmentCategory + " does not exist, create it first");
+            }
+        }
+        for (MuscleGroupRequest muscleGroupRequest : adminExerciseRequest.getMuscleGroups()) {
+            String name = muscleGroupRequest.getName();
+            if (muscleGroupCategoryRepository.findMuscleGroupCategoryByName(name).isEmpty()) {
+                throw new Exception("Category " + name + " does not exist, create it first");
+            }
+        }
+
+        Exercise newExercise = exerciseMapper.toExercise(adminExerciseRequest);
+        exerciseRepository.save(newExercise);
     }
 }

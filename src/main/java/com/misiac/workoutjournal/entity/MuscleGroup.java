097f7ecb276a16c3723c3f9a7aa5edcdc8751dce
Misiac <Misiac2000@gmail.com>
2023-12-17 02:13:39 +0100

    add equals and hashcode methods to MuscleGroup

--- src/main/java/com/misiac/workoutjournal/entity/MuscleGroup.java
+++ src/main/java/com/misiac/workoutjournal/entity/MuscleGroup.java
@@ -4,6 +4,8 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.Objects;
+
 @Getter
 @Setter
 @Entity
@@ -25,4 +27,23 @@ public class MuscleGroup {
     @Column(name = "is_primary")
     private Byte isPrimary;
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        MuscleGroup that = (MuscleGroup) o;
+
+        if (!exercise.equals(that.exercise)) return false;
+        if (!Objects.equals(category, that.category)) return false;
+        return Objects.equals(isPrimary, that.isPrimary);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = exercise.hashCode();
+        result = 31 * result + (category != null ? category.hashCode() : 0);
+        result = 31 * result + (isPrimary != null ? isPrimary.hashCode() : 0);
+        return result;
+    }
 }

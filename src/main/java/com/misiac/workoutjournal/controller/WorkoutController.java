3cd873425e1426e7a535f6feff2877e1d5cfa854
Misiac <Misiac2000@gmail.com>
2023-12-19 06:30:59 +0100

    add delete mapping to controller for workout deletion

--- src/main/java/com/misiac/workoutjournal/controller/WorkoutController.java
+++ src/main/java/com/misiac/workoutjournal/controller/WorkoutController.java
@@ -6,6 +6,8 @@ import com.misiac.workoutjournal.service.WorkoutService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -35,6 +37,13 @@ public class WorkoutController {
     public Page<Workout> getWorkoutsForUser(@RequestHeader(value = "Authorization") String token, Pageable pageable) {
         String email = "testuser@email.com";
         return workoutService.getWorkoutsForUser(email, pageable);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteWorkout(@RequestHeader(value = "Authorization") String token, @PathVariable(name = "id") Long workoutId) throws Exception {
+        String email = "testuser@email.com";
+        workoutService.deleteWorkout(email, workoutId);
+        return new ResponseEntity<>("xD", HttpStatus.OK);
     }
 
 }

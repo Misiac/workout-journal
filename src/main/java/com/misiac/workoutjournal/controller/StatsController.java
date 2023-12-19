689ade5e7500ed3a40440e8ee96f3979e8dcf374
Misiac <Misiac2000@gmail.com>
2023-12-19 21:34:41 +0100

    add StatsController

---
+++ src/main/java/com/misiac/workoutjournal/controller/StatsController.java
@@ -0,0 +1,10 @@
+package com.misiac.workoutjournal.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+//@CrossOrigin
+@RestController
+@RequestMapping("/api/stats")
+public class StatsController {
+}

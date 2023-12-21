0927c4ce4433ea00925a398c415881b92444f079
Misiac <Misiac2000@gmail.com>
2023-12-21 20:24:17 +0100

    add remaining exception handlers

--- src/main/java/com/misiac/workoutjournal/handler/ExceptionsHandler.java
+++ src/main/java/com/misiac/workoutjournal/handler/ExceptionsHandler.java
@@ -1,6 +1,8 @@
 package com.misiac.workoutjournal.handler;
 
+import com.misiac.workoutjournal.exception.EntityAlreadyExistsException;
 import com.misiac.workoutjournal.exception.EntityDoesNotExistException;
+import com.misiac.workoutjournal.exception.UnauthorizedException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -12,5 +14,15 @@ public class ExceptionsHandler {
     @ExceptionHandler
     public ResponseEntity<String> handleException(EntityDoesNotExistException e) {
         return new ResponseEntity<>(e.getMessage(), HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<String> handleException(EntityAlreadyExistsException e) {
+        return new ResponseEntity<>(e.getMessage(), HttpStatus.CONFLICT);
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<String> handleException(UnauthorizedException e) {
+        return new ResponseEntity<>(e.getMessage(), HttpStatus.UNAUTHORIZED);
     }
 }

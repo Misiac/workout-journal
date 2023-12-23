1de24a454378eaa028c2345053fa07b5a98b9a09
Misiac <Misiac2000@gmail.com>
2023-12-23 22:25:10 +0100

    add WorkoutRepositoryTest

--- src/test/java/com/misiac/workoutjournal/repository/WorkoutRepositoryTest.java
+++ src/test/java/com/misiac/workoutjournal/repository/WorkoutRepositoryTest.java
@@ -1,15 +1,22 @@
 package com.misiac.workoutjournal.repository;
 
+import com.misiac.workoutjournal.entity.Exercise;
 import com.misiac.workoutjournal.entity.User;
 import com.misiac.workoutjournal.entity.Workout;
+import com.misiac.workoutjournal.entity.WorkoutExercise;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.jdbc.EmbeddedDatabaseConnection;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 import java.time.LocalDateTime;
+import java.util.LinkedList;
 
 @DataJpaTest
 @AutoConfigureTestDatabase(connection = EmbeddedDatabaseConnection.H2)
@@ -19,19 +26,17 @@ class WorkoutRepositoryTest {
     private WorkoutRepository workoutRepository;
     @Autowired
     private UserRepository userRepository;
+    @Autowired
+    ExerciseRepository exerciseRepository;
+
+    public static final String TEST_EMAIL = "test@email.com";
 
 
     @Test
+    @DisplayName("Saving workout with Identity Generation ID")
     public void testSaveWorkoutBlank() {
 
-        User user = new User();
-        user.setEmail("test@email.com");
-
-        Workout workout = Workout.builder()
-                .user(user)
-                .date(LocalDateTime.now())
-                .build();
-        userRepository.save(user);
+        Workout workout = constructTestWorkout();
         workoutRepository.save(workout);
 
         Workout savedWorkout = workoutRepository.findById(workout.getId()).orElse(null);
@@ -41,4 +46,78 @@ class WorkoutRepositoryTest {
         Assertions.assertEquals(workout.getDate(), savedWorkout.getDate());
     }
 
+    @Test
+    @DisplayName("Cascade: saving workout without saving its exercises")
+    public void testWorkoutExercisesCascade() {
+
+        Exercise exercise = new Exercise();
+        exercise.setName("Lat Raise");
+        exerciseRepository.save(exercise);
+
+        Workout workout = constructTestWorkout();
+
+        for (int i = 1; i < 3; i++) {
+            WorkoutExercise we = WorkoutExercise.builder()
+                    .exerciseType(exercise)
+                    .load(2.5f)
+                    .reps(5)
+                    .setNumber(i)
+                    .parentWorkout(workout)
+                    .build();
+            workout.getWorkoutExercises().add(we);
+
+        }
+        workoutRepository.save(workout);
+        Workout savedWorkout = workoutRepository.findById(workout.getId()).orElseThrow();
+        Assertions.assertEquals(2, savedWorkout.getWorkoutExercises().size());
+    }
+
+    @Test
+    @DisplayName("Custom searching with Pageable and User Email Desc")
+    public void testCustomSearchingWithUserEmailAndPageable() throws InterruptedException {
+
+        Workout workout1 = constructTestWorkout();
+        Thread.sleep(500);
+        Workout workout2 = constructTestWorkout();
+        workoutRepository.save(workout1);
+        workoutRepository.save(workout2);
+
+        Pageable pageable = Pageable.ofSize(5);
+        Page<Workout> workouts = workoutRepository.findWorkoutsByUserEmailOrderByDateDesc(TEST_EMAIL, pageable);
+
+        Assertions.assertEquals(2, workouts.getTotalElements());
+        Assertions.assertEquals(TEST_EMAIL, workouts.getContent().getFirst().getUser().getEmail());
+        Assertions.assertTrue(
+                workouts.getContent().getFirst().getDate().isAfter(
+                        workouts.getContent().getLast().getDate())
+        );
+    }
+
+    @Test
+    @DisplayName("Saving workout without user throws Exception")
+    public void testSavingWorkoutWithoutUser() {
+
+        Workout workout = Workout.builder()
+                .date(LocalDateTime.now())
+                .workoutExercises(new LinkedList<>())
+                .build();
+
+        Assertions.assertThrows(DataIntegrityViolationException.class,
+                () -> workoutRepository.save(workout)
+        );
+    }
+
+    private Workout constructTestWorkout() {
+        User user = new User();
+        user.setEmail(TEST_EMAIL);
+
+        Workout workout = Workout.builder()
+                .user(user)
+                .date(LocalDateTime.now())
+                .workoutExercises(new LinkedList<>())
+                .build();
+        userRepository.save(user);
+
+        return workout;
+    }
 }

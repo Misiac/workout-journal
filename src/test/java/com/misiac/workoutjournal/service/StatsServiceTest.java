6591de55ed5df459525d9397c647ce8a31ec9816
Misiac <Misiac2000@gmail.com>
2023-12-26 14:41:41 +0100

    finish StatsService tests

--- src/test/java/com/misiac/workoutjournal/service/StatsServiceTest.java
+++ src/test/java/com/misiac/workoutjournal/service/StatsServiceTest.java
@@ -4,10 +4,12 @@ import com.misiac.workoutjournal.entity.User;
 import com.misiac.workoutjournal.entity.Workout;
 import com.misiac.workoutjournal.entity.WorkoutExercise;
 import com.misiac.workoutjournal.repository.UserRepository;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.List;
@@ -26,7 +28,89 @@ class StatsServiceTest {
     private StatsService statsService;
 
     @Test
-    void getTotalSets() {
+    @DisplayName("getTotalSets normal conditions method call")
+    void testGetTotalSetsNormalConditions() {
+        mockNormalConditionsUser();
+        Long result = statsService.getTotalSets("email");
+
+        assertEquals(2, result);
+    }
+
+    @Test
+    @DisplayName("getTotalSets zero workouts")
+    void testGetTotalSetsZeroWorkouts() {
+        User user = new User();
+
+        when(userRepository.findUserByEmail("email")).thenReturn(user);
+        Long result = statsService.getTotalSets("email");
+
+        assertEquals(0, result);
+    }
+
+    @Test
+    @DisplayName("getTotalVolume normal conditions method call")
+    void testGetTotalVolumeNormalConditions() {
+        mockNormalConditionsUser();
+        Double result = statsService.getTotalVolume("email");
+
+        assertEquals(33, result, 0.01);
+    }
+
+    @Test
+    @DisplayName("getTotalVolume zero workouts")
+    void testGetTotalVolumeZeroWorkouts() {
+        User user = new User();
+
+        when(userRepository.findUserByEmail("email")).thenReturn(user);
+        Double result = statsService.getTotalVolume("email");
+
+        assertEquals(0, result);
+    }
+
+    @Test
+    @DisplayName("getTotalWorkouts normal conditions method call")
+    void testGetTotalWorkoutsNormalConditions() {
+        mockNormalConditionsUser();
+
+        Long result = statsService.getTotalWorkouts("email");
+
+        assertEquals(1, result);
+    }
+
+    @Test
+    @DisplayName("getTotalWorkouts zero workouts")
+    void testGetTotalWorkoutsZeroWorkouts() {
+        User user = new User();
+
+        when(userRepository.findUserByEmail("email")).thenReturn(user);
+        Long result = statsService.getTotalWorkouts("email");
+
+        assertEquals(0, result);
+    }
+
+    @Test
+    @DisplayName("getTotalReps normal conditions method call")
+    void testGetTotalRepsNormalConditions() {
+        mockNormalConditionsUser();
+        Long result = statsService.getTotalReps("email");
+
+        assertEquals(6, result);
+    }
+
+    @Test
+    @DisplayName("getTotalReps normal zero workouts")
+    void testGetTotalRepsZeroWorkouts() {
+        User user = new User();
+
+        when(userRepository.findUserByEmail("email")).thenReturn(user);
+
+        Long result = statsService.getTotalReps("email");
+
+        assertEquals(0, result);
+    }
+
+
+    private void mockNormalConditionsUser() {
         User user = new User();
         WorkoutExercise workoutExercise = new WorkoutExercise();
         WorkoutExercise workoutExercise2 = new WorkoutExercise();
@@ -39,13 +123,9 @@ class StatsServiceTest {
 
         Workout workout = new Workout();
 
-        workout.setWorkoutExercises(List.of(workoutExercise,workoutExercise2));
+        workout.setWorkoutExercises(List.of(workoutExercise, workoutExercise2));
         user.setWorkouts(Set.of(workout));
 
-        when(userRepository.findUserByEmail("email")).thenReturn(user);
-        Long result = statsService.getTotalSets("email");
-
-        assertEquals(2,result);
-
+        when(userRepository.findUserByEmail(Mockito.anyString())).thenReturn(user);
     }
 }

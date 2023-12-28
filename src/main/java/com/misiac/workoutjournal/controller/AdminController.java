0e78c5b7bdd8b6767bb5d5c26717beba92df2cde
Misiac <Misiac2000@gmail.com>
2023-12-28 18:16:07 +0100

    add dependency for AdminValidator (now Spring Bean!)

--- src/main/java/com/misiac/workoutjournal/controller/AdminController.java
+++ src/main/java/com/misiac/workoutjournal/controller/AdminController.java
@@ -3,12 +3,12 @@ package com.misiac.workoutjournal.controller;
 import com.misiac.workoutjournal.exception.UnauthorizedException;
 import com.misiac.workoutjournal.requestmodels.AdminCreateExerciseRequest;
 import com.misiac.workoutjournal.service.AdminService;
+import com.misiac.workoutjournal.util.AdminValidator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import static com.misiac.workoutjournal.util.AdminValidator.validateAdmin;
 import static com.misiac.workoutjournal.util.MessageProvider.*;
 
 @CrossOrigin("http://localhost:3000")
@@ -18,15 +18,17 @@ import static com.misiac.workoutjournal.util.MessageProvider.*;
 public class AdminController {
 
     private final AdminService adminService;
+    private final AdminValidator adminValidator;
 
     @Autowired
-    public AdminController(AdminService adminService) {
+    public AdminController(AdminService adminService, AdminValidator adminValidator) {
         this.adminService = adminService;
+        this.adminValidator = adminValidator;
     }
 
     @PostMapping("/muscle-category")
     public ResponseEntity<String> addMuscleGroupCategory(@RequestHeader(value = "Authorization") String token, @RequestParam String name) {
-        if (validateAdmin(token)) {
+        if (adminValidator.validateAdmin(token)) {
             adminService.addMuscleGroupCategory(name);
             return new ResponseEntity<>(CATEGORY_CREATED, HttpStatus.CREATED);
         } else {
@@ -36,7 +38,7 @@ public class AdminController {
 
     @PostMapping("/equipment-category")
     public ResponseEntity<String> addEquipmentCategory(@RequestHeader(value = "Authorization") String token, @RequestParam String name) {
-        if (validateAdmin(token)) {
+        if (adminValidator.validateAdmin(token)) {
             adminService.addEquipmentCategory(name);
             return new ResponseEntity<>(CATEGORY_CREATED, HttpStatus.CREATED);
         } else {
@@ -47,7 +49,7 @@ public class AdminController {
     @PostMapping("/exercise")
     public ResponseEntity<String> addExercise(@RequestHeader(value = "Authorization") String token,
                                               @RequestBody AdminCreateExerciseRequest adminExerciseRequest) {
-        if (validateAdmin(token)) {
+        if (adminValidator.validateAdmin(token)) {
             adminService.addExercise(adminExerciseRequest);
             return new ResponseEntity<>(EXERCISE_CREATED, HttpStatus.CREATED);
         } else {
@@ -59,7 +61,7 @@ public class AdminController {
     public ResponseEntity<String> bindEquipmentCategory
             (@RequestHeader(value = "Authorization") String token, @PathVariable Long exerciseId,
              @PathVariable String categoryName) {
-        if (validateAdmin(token)) {
+        if (adminValidator.validateAdmin(token)) {
             adminService.bindEquipmentCategory(exerciseId, categoryName);
             return new ResponseEntity<>(CATEGORY_BOUND, HttpStatus.OK);
         } else {
@@ -70,7 +72,7 @@ public class AdminController {
     @DeleteMapping("/exercise/{exerciseId}/equipment-categories/{categoryName}")
     public ResponseEntity<String> unbindEquipmentCategory(@RequestHeader(value = "Authorization") String token,
                                                           @PathVariable Long exerciseId, @PathVariable String categoryName) {
-        if (validateAdmin(token)) {
+        if (adminValidator.validateAdmin(token)) {
             adminService.unbindEquipmentCategory(exerciseId, categoryName);
             return new ResponseEntity<>(CATEGORY_UNBOUND, HttpStatus.OK);
         } else {
@@ -82,7 +84,7 @@ public class AdminController {
     public ResponseEntity<String> bindMuscleCategory(@RequestHeader(value = "Authorization") String token,
                                                      @PathVariable Long exerciseId, @PathVariable String categoryName,
                                                      @RequestParam boolean isPrimary) {
-        if (validateAdmin(token)) {
+        if (adminValidator.validateAdmin(token)) {
             adminService.bindMuscleCategory(exerciseId, categoryName, isPrimary);
             return new ResponseEntity<>(CATEGORY_BOUND, HttpStatus.OK);
         } else {
@@ -94,7 +96,7 @@ public class AdminController {
     public ResponseEntity<String> unbindMuscleCategory(@RequestHeader(value = "Authorization") String token,
                                                        @PathVariable Long exerciseId,
                                                        @PathVariable String categoryName) {
-        if (validateAdmin(token)) {
+        if (adminValidator.validateAdmin(token)) {
             adminService.unbindMuscleCategory(exerciseId, categoryName);
             return new ResponseEntity<>(CATEGORY_UNBOUND, HttpStatus.OK);
         } else {

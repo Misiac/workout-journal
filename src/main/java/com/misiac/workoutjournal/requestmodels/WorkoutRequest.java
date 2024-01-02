2fed25807f6b78954baa3bf7a2ea30a165dad0c8
Misiac <Misiac2000@gmail.com>
2024-01-02 22:18:01 +0100

    add constructors annotations in WorkoutRequest for easier testing

--- src/main/java/com/misiac/workoutjournal/requestmodels/WorkoutRequest.java
+++ src/main/java/com/misiac/workoutjournal/requestmodels/WorkoutRequest.java
@@ -1,12 +1,16 @@
 package com.misiac.workoutjournal.requestmodels;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
 @Getter
+@AllArgsConstructor
+@NoArgsConstructor
 public class WorkoutRequest {
 
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")

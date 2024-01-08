c0ff74f3d04f3e40041de391b3eeb2a99348a4ae
Misiac <Misiac2000@gmail.com>
2024-01-08 15:34:11 +0100

    remove IncorrectInputException, now Hibernate Validator handles it

--- wj-backend/src/main/java/com/misiac/workoutjournal/handler/ExceptionsHandler.java
+++ wj-backend/src/main/java/com/misiac/workoutjournal/handler/ExceptionsHandler.java
@@ -2,7 +2,6 @@ package com.misiac.workoutjournal.handler;
 
 import com.misiac.workoutjournal.exception.EntityAlreadyExistsException;
 import com.misiac.workoutjournal.exception.EntityDoesNotExistException;
-import com.misiac.workoutjournal.exception.IncorrectInputException;
 import com.misiac.workoutjournal.exception.UnauthorizedException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -25,10 +24,5 @@ public class ExceptionsHandler {
     @ExceptionHandler
     public ResponseEntity<String> handleException(UnauthorizedException e) {
         return new ResponseEntity<>(e.getMessage(), HttpStatus.UNAUTHORIZED);
-    }
-
-    @ExceptionHandler
-    public ResponseEntity<String> handleException(IncorrectInputException e) {
-        return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
     }
 }

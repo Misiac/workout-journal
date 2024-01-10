37ef5abe735618e0d4d3f3190dc7083db1e6f0d7
Misiac <Misiac2000@gmail.com>
2024-01-10 19:41:18 +0100

    pass reloadTrigger as a prop to refresh list of exercises in ManageExerciseBindings.tsx

--- wj-frontend/src/layouts/AdminPage/AdminPage.tsx
+++ wj-frontend/src/layouts/AdminPage/AdminPage.tsx
@@ -1,23 +1,23 @@
 import Navbar from "../Navbar";
-import React from "react";
+import React, {useState} from "react";
 import CreateExercise from "./Components/CreateExercise";
 import CreateNewCategory from "./Components/CreateNewCategory";
 import ManageExerciseBindings from "./Components/ManageExerciseBindings";
 
 
 export const AdminPage = () => {
+    const [reloadTrigger, setReloadTrigger] = useState(false);
     return (
         <>
             <Navbar/>
 
-
             <header className="bg-white shadow">
                 <div className="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-                    <CreateExercise/>
+                    <CreateExercise setReloadTrigger={setReloadTrigger} reloadTrigger={reloadTrigger}/>
                     <hr/>
                     <CreateNewCategory/>
                     <hr/>
-                    <ManageExerciseBindings/>
+                    <ManageExerciseBindings reloadTrigger={reloadTrigger}/>
                 </div>
             </header>
             <main>

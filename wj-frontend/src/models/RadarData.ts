c764061c227491698370753615a2c041a7747170
Misiac <Misiac2000@gmail.com>
2024-01-15 01:21:23 +0100

    add model for chart data

---
+++ wj-frontend/src/models/RadarData.ts
@@ -0,0 +1,19 @@
+class RadarData {
+    chest: number;
+    shoulders: number;
+    legs: number;
+    core: number;
+    back: number;
+    arms: number;
+
+    constructor(chest: number, shoulders: number, legs: number, core: number, back: number, arms: number) {
+        this.chest = chest;
+        this.shoulders = shoulders;
+        this.legs = legs;
+        this.core = core;
+        this.back = back;
+        this.arms = arms;
+    }
+}
+
+export default RadarData;

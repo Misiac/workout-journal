7150cfcbf7f9044dba9904f714f1c158ffbf1b42
Misiac <Misiac2000@gmail.com>
2024-01-15 01:19:43 +0100

    add backend support for radar chart data

--- wj-frontend/src/layouts/WorkoutsPage/Stats/Stats.tsx
+++ wj-frontend/src/layouts/WorkoutsPage/Stats/Stats.tsx
@@ -1,4 +1,24 @@
+import TotalVolumeCard from "./Components/TotalVolumeCard";
+import TotalRepsCard from "./Components/TotalRepsCard";
+import TotalSetsCard from "./Components/TotalSetsCard";
+import TotalWorkoutsCard from "./Components/TotalWorkoutsCard";
+import MuscleRadar from "./Components/MuscleRadar";
+
+
 export const Stats = () => {
 
+    return (
+        <div className="grid grid-cols-4 grid-rows-2 gap-6 py-6">
+            <div className=""><TotalRepsCard/></div>
+            <div className="col-start-1 row-start-2"><TotalSetsCard/></div>
+            <div className="col-start-2 row-start-1"><TotalVolumeCard/></div>
+            <div className="col-start-2 row-start-2"><TotalWorkoutsCard/></div>
+            <div className="col-span-2 row-span-3 col-start-3 row-start-1 items-center justify-center flex">
+                <MuscleRadar/>
+            </div>
+        </div>
+
+    );
 }
 export default Stats;
+

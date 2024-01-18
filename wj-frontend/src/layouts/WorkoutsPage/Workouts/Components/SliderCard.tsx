9e2160df94f3421fddb75a45a17e7f651bbca258
Misiac <Misiac2000@gmail.com>
2024-01-18 15:35:58 +0100

    round active bar

--- wj-frontend/src/layouts/WorkoutsPage/Workouts/Components/SliderCard.tsx
+++ wj-frontend/src/layouts/WorkoutsPage/Workouts/Components/SliderCard.tsx
@@ -4,7 +4,7 @@ export const SliderCard = () => {
     return (
         <div className="relative mx-auto max-w-md">
             <div className="bg-white p-3 rounded-md hover:bg-gray-100 border-gray-100 flex items-center">
-                <div className="w-1 h-10 bg-regal-blue mr-3 invisible" ></div>
+                <div className="w-1 h-10 bg-regal-blue mr-3 rounded-t-lg rounded-b-lg" ></div>
                 <div>
                     <h1 className="font-bold">Monday Morning Workout</h1>
                     <p className="">19.01.2024 19:54</p>

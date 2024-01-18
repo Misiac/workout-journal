9f0d6cff8dab8bcbd754da8f22b5bfb286ed934c
Misiac <Misiac2000@gmail.com>
2024-01-18 02:01:21 +0100

    prepare click mechanism for SliderCard.tsx

--- wj-frontend/src/layouts/WorkoutsPage/Workouts/Components/SliderCard.tsx
+++ wj-frontend/src/layouts/WorkoutsPage/Workouts/Components/SliderCard.tsx
@@ -2,14 +2,16 @@ import React from "react";
 
 export const SliderCard = () => {
     return (
-        <div
-            className="relative mx-auto max-w-md">
-            <div className="bg-white p-3 rounded-md ">
-                <h1 className="font-bold text-lg">Monday Morning Workout</h1>
-                <p className="">19.01.2024 19:54</p>
+        <div className="relative mx-auto max-w-md">
+            <div className="bg-white p-3 rounded-md hover:bg-gray-100 border-gray-100 flex items-center">
+                <div className="w-1 h-10 bg-regal-blue mr-3 invisible" ></div>
+                <div>
+                    <h1 className="font-bold">Monday Morning Workout</h1>
+                    <p className="">19.01.2024 19:54</p>
+                </div>
             </div>
-            <hr/>
         </div>
     );
 }
+
 export default SliderCard;
--- wj-frontend/src/layouts/WorkoutsPage/Workouts/Components/WorkoutsSlider.tsx
+++ wj-frontend/src/layouts/WorkoutsPage/Workouts/Components/WorkoutsSlider.tsx
@@ -4,17 +4,17 @@ import React from "react";
 export const WorkoutsSlider = () => {
     return (
 
-            <div className="scroll-container overflow-y-auto h-full w-1/5">
-                <div className="flex flex-col gap-4">
-                    <SliderCard/>
-                    <SliderCard/>
-                    <SliderCard/>
-                    <SliderCard/>
-                    <SliderCard/>
-                    <SliderCard/>
-                    <SliderCard/>
-                </div>
+        <div className="scroll-container overflow-y-auto h-full w-1/5 px-1 ">
+            <div className="flex flex-col gap-4">
+
+                <SliderCard/>
+                <SliderCard/>
+                <SliderCard/>
+                <SliderCard/>
+
+
             </div>
+        </div>
 
     );
 }
